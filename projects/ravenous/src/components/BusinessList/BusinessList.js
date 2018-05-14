797c72e06c403e6918ec55cb48358183dc6ac0c2
Aditya Goyal <adi@Adityas-MacBook-Pro.local>
2018-05-15 06:31:34 +1000

    ravenous part III

--- projects/ravenous/src/components/BusinessList/BusinessList.js
+++ projects/ravenous/src/components/BusinessList/BusinessList.js
@@ -6,12 +6,11 @@ class BusinessList extends React.Component{
   render() {
     return(
       <div className="BusinessList">
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
+      {
+        this.props.businesses.map( (business, index) => {
+          return <Business business={business} key={'business-' + index}/>
+        })
+      }
     </div>
     );
   }
